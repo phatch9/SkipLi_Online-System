a0a1e221a5b7bbaf79a35e3117df02abe7eed6db
Phat <phat.chauu26@gmail.com>
2025-06-05 13:21:57 -0700

    firebase and sms service

--- design/src/utils/sendPhoneNumber.js
+++ design/src/utils/sendPhoneNumber.js
@@ -1,11 +1,12 @@
 import axios from "axios";
 let sendPhoneNumber = async (phoneNumber) => {
-  try {
-    return await axios.post("http://localhost:8000/newAccessCode", {
-      phoneNumber,
-    });
-  } catch (e) {
-    throw new Error(e.message);
-  }
-};
+    try {
+        return await axios.post("http://localhost:8000/newAccessCode", {
+        phoneNumber,
+        });
+    }   
+        catch (e) {
+            throw new Error(e.message);
+        }
+    };
 export default sendPhoneNumber;
