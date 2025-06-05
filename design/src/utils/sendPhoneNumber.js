66f22ecfb6b928b48e5239ff9f8a967d54545472
Phat <phat.chauu26@gmail.com>
2025-06-05 00:57:43 -0700

    initialized backend:

---
+++ design/src/utils/sendPhoneNumber.js
@@ -0,0 +1,11 @@
+import axios from "axios";
+let sendPhoneNumber = async (phoneNumber) => {
+  try {
+    return await axios.post("http://localhost:8000/newAccessCode", {
+      phoneNumber,
+    });
+  } catch (e) {
+    throw new Error(e.message);
+  }
+};
+export default sendPhoneNumber;
